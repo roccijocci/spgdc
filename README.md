fbf4d2871c0904b5cb464d9e3941e18e69315586
jaack <jags4real25@gmail.com>
2022-04-25 23:46:15 +0100

    rm feature/datamodel

---
+++ README.md
@@ -0,0 +1,5 @@
+# spgdc
+
+### was bored thought is should build SPG's Discipleship class API. 
+
+#### can't hurt right 😉😉
--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Discipleship struct {
+	Date            time.Time `json: "date"`
+	Topic           string    `json: "topic"`
+	Text            string    `json: "text"`
+	MemoryVerse     string    `json: "memoryverse"`
+	Introduction    string    `json: "introduction"`
+	LessonOutline   string    `json: "lessonoutline"`
+	Questions       string    `json: "questions"`
+	LifeApplication string    `json: "lifeapplication"`
+}
 
 func main() {
 	router := gin.Default()
