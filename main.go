0262d5f8de47dfee4c07f6e475a5852b82b25b1b
jaack <jags4real25@gmail.com>
2022-04-28 00:36:47 +0100

    upadate gc endpoint

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"time"
 
@@ -25,6 +27,8 @@ var discipleshipClasses []DiscipleshipClass
 
 func init() {
 	discipleshipClasses = make([]DiscipleshipClass, 0)
+	file, _ := ioutil.ReadFile("discipleshipclass.json")
+	_ = json.Unmarshal([]byte(file), &discipleshipClasses)
 }
 
 func NewDCHandler(c *gin.Context) {
@@ -43,13 +47,40 @@ func NewDCHandler(c *gin.Context) {
 
 }
 
+//handler endpoint lists
 func ListDCHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, discipleshipClasses)
+}
+
+//updates
+func UpdateDCHandler(c *gin.Context) {
+	id := c.Param("id")
+	var discipleshipClass DiscipleshipClass
+
+	if err := c.ShouldBindJSON(&discipleshipClass); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	index := -1
+	for i := 0; i < len(discipleshipClasses); i++ {
+		if discipleshipClasses[i].Id == id {
+			index = i
+		}
+	}
+	if index == 1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Discipleship class Memo not found"})
+		return
+	}
+	discipleshipClasses[index] = discipleshipClass
+	c.JSON(http.StatusOK, discipleshipClass)
 }
 
 func main() {
 	router := gin.Default()
 	router.POST("/dc", NewDCHandler)
 	router.GET("/dc", ListDCHandler)
+	router.PUT("/dc/:id", UpdateDCHandler)
 	router.Run()
 }
