872937bc2887ed1c34cac13112cc5aaefc857ef3
jaack <jags4real25@gmail.com>
2022-05-19 00:06:28 +0100

    does not insert at all: broken

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/xid"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type DiscipleshipClass struct {
-	Id              string    `json: "id"`
+	Id              string    `json: "_id"`
 	Date            string    `json: "date"`
 	Topic           string    `json: "topic"`
 	Text            []string  `json: "text"`
@@ -24,11 +32,36 @@ type DiscipleshipClass struct {
 }
 
 var discipleshipClasses []DiscipleshipClass
+var ctx context.Context
+var err error
+var client *mongo.Client
 
 func init() {
 	discipleshipClasses = make([]DiscipleshipClass, 0)
 	file, _ := ioutil.ReadFile("discipleshipclass.json")
 	_ = json.Unmarshal([]byte(file), &discipleshipClasses)
+
+	ctx = context.Background()
+	client, err = mongo.Connect(ctx,
+		options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err = client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to MongoDB")
+
+	var listOfDiscipleshipClasses []interface{}
+	for _, dsc := range discipleshipClasses {
+		listOfDiscipleshipClasses = append(listOfDiscipleshipClasses, dsc)
+	}
+	collection := client.Database(os.Getenv(
+		"MONGO_DATABASE")).Collection("discipleshipclasses")
+	InsertManyResult, err := collection.InsertMany(ctx, listOfDiscipleshipClasses)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(err)
+	}
+	log.Println("Inserted DiscipleshipClass:",
+		len(InsertManyResult.InsertedIDs))
 }
 
 func NewDCHandler(c *gin.Context) {
@@ -79,7 +112,7 @@ func UpdateDCHandler(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
-	router.POST("/dc", NewDCHandler)
+	//router.POST("/dc", NewDCHandler)
 	router.GET("/dc", ListDCHandler)
 	router.PUT("/dc/:id", UpdateDCHandler)
 	router.Run()
